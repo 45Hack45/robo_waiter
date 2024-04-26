e6dbd080042159914b496baf9d73584e44d7f0f3
Daniel <danird2305@gmail.com>
2024-04-26 12:17:27 +0200

    Sensores

--- Raspberry_Pi_Ultrasonic_Sensor.py
+++ Raspberry_Pi_Ultrasonic_Sensor.py
@@ -1,10 +1,14 @@
 from gpiozero import DistanceSensor, LED
 from signal import pause
 
-sensor = DistanceSensor(27, 22, max_distance=2, threshold_distance=1.5)
+sensor1 = DistanceSensor(27, 22, max_distance=2, threshold_distance=1.5)
+sensor2 = DistanceSensor(4, 17, max_distance=2, threshold_distance=1.5)
 led = LED(26)
 
-sensor.when_in_range = led.on
-sensor.when_out_of_range = led.off
+sensor1.when_in_range = led.on
+sensor1.when_out_of_range = led.off
+
+sensor2.when_in_range = led.on
+sensor2.when_out_of_range = led.off
 
 pause()
