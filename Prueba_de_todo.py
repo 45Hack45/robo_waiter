6898fa2122c84c42308ad2028c4a402df614f258
Adrian020 <101559244+Adrian020@users.noreply.github.com>
2024-05-15 12:28:25 +0200

    Update Prueba_de_todo.py

--- Prueba_de_todo.py
+++ Prueba_de_todo.py
@@ -1,12 +1,20 @@
-from gpiozero import Robot, Motor, DistanceSensor, Button, MCP3008
+from gpiozero import PWMOutputDevice, DistanceSensor, Button, MCP3008
 from time import pause, sleep
 
+PWM_FORWARD_LEFT_PIN = 26
+PWM_REVERSE_LEFT_PIN = 19
+PWM_FORWARD_RIGHT_PIN = 13
+PWM_REVERSE_RIGHT_PIN = 6
 adc = MCP3008(channel=0)
-robot = Robot(left=Motor(26, 19), right=Motor(13, 6))
 sensor1 = DistanceSensor(27, 22, max_distance=1, threshold_distance=0.3)
 sensor2 = DistanceSensor(23, 17, max_distance=1, threshold_distance=0.3)
 boton = Button(16)
 Boton_on = False
+
+forwardLeft = PWMOutputDevice(PWM_FORWARD_LEFT_PIN, frequency=1000)  
+reverseLeft = PWMOutputDevice(PWM_REVERSE_LEFT_PIN, frequency=1000)    
+forwardRight = PWMOutputDevice(PWM_FORWARD_RIGHT_PIN, frequency=1000)  
+reverseRight = PWMOutputDevice(PWM_REVERSE_RIGHT_PIN, frequency=1000)
 
 def leer_valor_adc():
     valor_adc = adc.value
@@ -16,7 +24,24 @@ def leer_valor_adc():
 def cambio():
     global Boton_on
     Boton_on = not Boton_on
+    
+def allStop():  	
+    forwardLeft.value = 0  	
+    reverseLeft.value = 0  	
+    forwardRight.value = 0  	
+    reverseRight.value = 0 
+    
+def forwardDrive():  	
+    forwardLeft.value = 1.0  	
+    reverseLeft.value = 0  	
+    forwardRight.value = 1.0 
+    reverseRight.value = 0 
 
+def reverseDrive():  	
+    forwardLeft.value = 0 
+    reverseLeft.value = 1.0  	
+    forwardRight.value = 0  	
+    reverseRight.value = 1.0  
 
 boton.when_activated = cambio()
 
@@ -24,10 +49,10 @@ x = 0
 
 while (x != 2):
     if (sensor1.distance < 0.3 or sensor2.distance < 0.3):
-        robot.forward()
+        forwardDrive()
         sleep(3)
     else:
-        robot.stop()
+        allStop()
         pause.until(sensor1.distance > 0.3 and sensor2.distance > 0.3) 
     sleep(5)
     if(Boton_on):
@@ -35,10 +60,10 @@ while (x != 2):
             voltaje = leer_valor_adc()
     sleep(1)
     if (sensor1.distance < 0.3 or sensor2.distance < 0.3):
-        robot.backward()
+        reverseDrive()
         sleep(3)
     else:
-        robot.stop()
+        allStop()
         pause.until(sensor1.distance > 0.3 and sensor2.distance > 0.3)
     sleep(5)
     if(Boton_on):
