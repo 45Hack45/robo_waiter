9e5f2a09f32859e2f95346bec4cf9167f5047702
Adrian020 <101559244+Adrian020@users.noreply.github.com>
2024-05-15 12:25:35 +0200

    Update Motores_sensor.py

--- Motores_sensor.py
+++ Motores_sensor.py
@@ -1,9 +1,29 @@
-from gpiozero import Robot, Motor, DistanceSensor
+from gpiozero import PWMOutputDevice, DistanceSensor
 from time import pause
 
-robot = Robot(left=Motor(26, 19), right=Motor(13, 6))
+PWM_FORWARD_LEFT_PIN = 26
+PWM_REVERSE_LEFT_PIN = 19
+PWM_FORWARD_RIGHT_PIN = 13
+PWM_REVERSE_RIGHT_PIN = 6
 sensor1 = DistanceSensor(27, 22, max_distance=1, threshold_distance=0.4)
 
-sensor1.when_in_range = robot.stop()
-sensor1.when_out_of_range = robot.forward()
+forwardLeft = PWMOutputDevice(PWM_FORWARD_LEFT_PIN, frequency=1000)  
+reverseLeft = PWMOutputDevice(PWM_REVERSE_LEFT_PIN, frequency=1000)    
+forwardRight = PWMOutputDevice(PWM_FORWARD_RIGHT_PIN, frequency=1000)  
+reverseRight = PWMOutputDevice(PWM_REVERSE_RIGHT_PIN, frequency=1000)
+
+def allStop():  	
+    forwardLeft.value = 0  	
+    reverseLeft.value = 0  	
+    forwardRight.value = 0  	
+    reverseRight.value = 0 
+  
+def forwardDrive():  	
+    forwardLeft.value = 1.0  	
+    reverseLeft.value = 0  	
+    forwardRight.value = 1.0 
+    reverseRight.value = 0  
+
+sensor1.when_in_range = allStop()
+sensor1.when_out_of_range = forwardDrive()
 pause()
