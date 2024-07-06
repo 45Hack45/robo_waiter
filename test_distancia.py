a66518de7fb256227a3914568db824c0df1bb336
Hack45 <45haha45@gmail.com>
2024-07-06 18:01:14 +0200

    test_distancia

--- test_distancia.py
+++ test_distancia.py
@@ -7,4 +7,4 @@ sensor2 = DistanceSensor(23, 17, max_distance=1, threshold_distance=0.5)
 while True:
     print('Distance 1 to nearest object is', sensor1.distance, 'm')
     print('Distance 2 to nearest object is', sensor2.distance, 'm')
-    sleep(1)
+    sleep(0.1)
