748b783104b24a8954f188f7fea3e56c32eecf39
Hack45 <45haha45@gmail.com>
2024-07-06 17:58:49 +0200

    test_distancia

--- test_distancia.py
+++ test_distancia.py
@@ -1,8 +1,10 @@
 from gpiozero import DistanceSensor
 from time import sleep
 
-sensor = DistanceSensor(27, 22, max_distance=1, threshold_distance=0.5)
+sensor1 = DistanceSensor(27, 22, max_distance=1, threshold_distance=0.5)
+sensor2 = DistanceSensor(23, 17, max_distance=1, threshold_distance=0.5)
 
 while True:
-    print('Distance to nearest object is', sensor.distance, 'm')
+    print('Distance 1 to nearest object is', sensor1.distance, 'm')
+    print('Distance 2 to nearest object is', sensor2.distance, 'm')
     sleep(1)
